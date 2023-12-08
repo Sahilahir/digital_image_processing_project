76f6b6336ebad743dc0f175ce162e9f3044ec4d4
Sahilahir <sahilahir333@gmail.com>
2023-12-08 17:20:44 -0600

    minor changes 2

--- DeOldify/colorize_image.py
+++ DeOldify/colorize_image.py
@@ -6,8 +6,8 @@ from deoldify.visualize import *
 torch.backends.cudnn.benchmark = True
 
 # Specify the path to your black and white restored image
-input_path = Path("C:/Semester4/digital_image_processing/dip_project/images/Restored_denoised_image.jpg")
+input_path = Path("C:/Semester4/digital_image_processing/dip_project/restored_new_images/bicunic_resolution_new_image.jpg")
 
 # Colorize the image and store the result in a results folder
 colorizer = get_image_colorizer(artistic=True)
-colorizer.plot_transformed_image(input_path, results_dir=Path('C:/Semester4/digital_image_processing/dip_project/result_images'), watermarked=False)
+colorizer.plot_transformed_image(input_path, results_dir=Path('C:/Semester4/digital_image_processing/dip_project/'), watermarked=False)
